4ae6ed43a3bbf60656adadce09a440c8f4001cd2
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-10 19:09:58 -0600

    express interfaces and example with index route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import type { TypedRequestBody, TypedResponse } from "interfaces";
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("index route hit"));
+//Index route: format for consistent implementation of interface and type. 
+//TODO: Interfaces for response shape, abstract response validation from interface for each endpoint.
+app.get("/", (req: Express.Request, res:TypedResponse<{response: string}>) => res.json({"response" : "This is a response"}));
 
 const port = process.env.PORT || 8080
 app.listen(port, () => {
