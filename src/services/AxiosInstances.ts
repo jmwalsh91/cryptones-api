7e5251823c1a8936b6c8efb1a3850849a66e432e
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-13 02:24:31 -0600

    getOhclv function with hardcoded query

--- src/services/AxiosInstances.ts
+++ src/services/AxiosInstances.ts
@@ -1,8 +1,18 @@
-
 import axios, { AxiosInstance } from "axios";
-
-
+const alphaKey = process.env.APIKEY;
+//Axios instance for querying alphavantage api
 const alphavantage: AxiosInstance = axios.create({
-    baseURL: 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY',
-    timeout: 10000,
-  });
+  baseURL: "https://www.alphavantage.co/query?function=CRYPTO_INTRADAY",
+  timeout: 10000,
+});
+
+export const getOhclv = async () => {
+  const data = await alphavantage.get("", {
+    params: {
+      symbol: "BTC",
+      market: "USD",
+      interval: "5min",
+      apikey: alphaKey,
+    },
+  })
+};
