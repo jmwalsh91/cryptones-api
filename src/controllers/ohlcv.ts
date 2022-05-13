e1dd0029c2b0017ab198c5328abf9680e3b26d62
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-13 03:16:27 -0600

    TODOs, merge into dev

--- src/controllers/ohlcv.ts
+++ src/controllers/ohlcv.ts
@@ -3,10 +3,13 @@ import { getOhclv } from "../services/AxiosInstances";
 import { TypedRequestBody, TypedResponse } from "../types/interfaces";
 const express = require("express")
 
+//router for "open, high, low, close, volume" query
 const routerOhlcv = express.Router();
 
 routerOhlcv.get("", async (req: TypedRequestBody<Request>, res: TypedResponse<any>, err: Error) => {
     let data = await getOhclv()
+    //TODO: INTERCEPTOR to mutate data shape to conform to...
+    //TODO: ...interface for Ohclv 
     console.log(data)
     return res.send(data)
   }
--- src/services/AxiosInstances.ts
+++ src/services/AxiosInstances.ts
@@ -1,25 +1,30 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 const alphaKey = process.env.APIKEY;
+
 //Axios instance for querying alphavantage api
 const alphavantage: AxiosInstance = axios.create({
   baseURL: "https://www.alphavantage.co/query?function=CRYPTO_INTRADAY",
   timeout: 10000,
 });
 
+//get "open high close low volume"
+//TODO: interface for params object
+//TODO: also in /controllers/ohlcv: interceptor and interface for interceptor's return 
 export const getOhclv = async () => {
-    console.log(alphaKey)
-    
-  const data: AxiosResponse = await alphavantage.get("", {
-    params: {
-      symbol: "BTC",
-      market: "USD",
-      interval: "5min",
-      apikey: alphaKey,
-    },
-  })
-  .then(data => {
-      console.log(data.data)
-      return data
-  })
+  console.log(alphaKey);
+
+  const data: AxiosResponse = await alphavantage
+    .get("", {
+      params: {
+        symbol: "BTC",
+        market: "USD",
+        interval: "5min",
+        apikey: alphaKey,
+      },
+    })
+    .then((data) => {
+      console.log(data.data);
+      return data;
+    });
   //TODO: CATCH
 };
