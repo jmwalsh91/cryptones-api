fbd7523dddce70c9fb85d7f5a8bf6123cef0cb05
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-13 21:01:08 -0600

    formatDate and reshapeObject util functions

--- src/controllers/ohlcv.ts
+++ src/controllers/ohlcv.ts
@@ -1,18 +1,46 @@
-import { JsonableValue } from "ts-jest";
+import _ from "lodash"
 import { getOhclv } from "../services/AxiosInstances";
 import { TypedRequestBody, TypedResponse } from "../types/interfaces";
-const express = require("express")
+const express = require("express");
 
 //router for "open, high, low, close, volume" query
 const routerOhlcv = express.Router();
 
-routerOhlcv.get("", async (req: TypedRequestBody<Request>, res: TypedResponse<any>, err: Error) => {
-    let data = await getOhclv()
+routerOhlcv.get(
+  "",
+  async (
+    req: TypedRequestBody<Request>,
+    res: TypedResponse<any>,
+    err: Error
+  ) => {
+    let data = await getOhclv();
     //TODO: INTERCEPTOR to mutate data shape to conform to...
-    //TODO: ...interface for Ohclv 
-    console.log(data)
-    return res.send(data)
+    //TODO: ...interface for Ohclv
+    console.log(data);
+    return res.send(data);
   }
-  );
+);
+
+//TODO: refactor this function
+routerOhlcv.interceptors.response.use((response) => {
+  console.log("interceptor");
+  let target = response.data["Time Series Crypto (5min)"];
+
+  function formatRes(arrOne, arrTwo) {
+    let resFormatted
+    return (resFormatted = _.zip(arrOne, arrTwo));
+  }
+  const reqKeys = Object.keys(target).map((thing) => {
+    return Date.parse(thing);
+  });
+  const formattedOb = Object.values(target).map((thing) => {
+    return Object.values(thing);
+  });
+  let volArr = formattedOb.map((thing) => {
+    return thing.pop();
+  });
+  formatRes(reqKeys, formattedOb);
+  return (response = { resFormatted, volArr });
+});
 
 export default routerOhlcv;
---
+++ src/utils/reshape.ts
@@ -0,0 +1,14 @@
+
+
+export function formatDate (target: Array<object>) {
+const formattedDate: Array<number> = Object.keys(target).map((date) => {
+    return Date.parse(date);
+  });
+  return formattedDate
+}
+  export function reshapeObject (target: Array<object>) {
+   const objValues: Array<object> = Object.values(target).map((values) => {
+    return Object.values(values);
+  });
+  return objValues
+}
