59f9f9dad9b8565d31505c3dc58238ad99cd3870
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-25 00:21:58 -0600

    validation in ohlcv-with-params getter

--- src/services/AxiosInstances.ts
+++ src/services/AxiosInstances.ts
@@ -5,7 +5,8 @@ import {
   getVolumeArrayFromOhlcv,
   reshapeObject,
 } from "../utils/reshape";
-const _ = require("lodash")
+import { validateInterval, validateSymbol } from "../utils/validate";
+const _ = require("lodash");
 
 const alphaKey = process.env.APIKEY;
 
@@ -17,9 +18,8 @@ export const alphavantage: AxiosInstance = axios.create({
 
 //get "open high close low volume"
 //TODO: interface for params object
-//TODO: also in /controllers/ohlcv: interceptor and interface for interceptor's return 
+//TODO: also in /controllers/ohlcv: interceptor and interface for interceptor's return
 export const getOhclv = async () => {
-
   const data: AxiosResponse = await alphavantage
     .get("", {
       params: {
@@ -33,30 +33,34 @@ export const getOhclv = async () => {
       console.log(data.data);
       return data;
     });
-    return data 
+  return data;
   //TODO: CATCH
 };
 
 //OHLCV w/ PARAMS
-export const getParamsOhclv = async (symbol, interval) => {
+export const getParamsOhclv = async (symbol: string, interval: string) => {
+  //validate params
+  const validSymbol: Promise<boolean> = validateSymbol(symbol);
+  const validInterval: Promise<boolean> = validateInterval(interval);
 
-  const data: AxiosResponse = await alphavantage
-    .get("", {
-      params: {
-        symbol: 
-        market: "USD",
-        interval: "5min",
-        apikey: alphaKey,
-      },
-    })
-    .then((data) => {
-      console.log(data.data);
-      return data;
-    });
-    return data 
+  //if symbol is included in validSymbols array (returns true) and interval is included in validInterval array (returns true), use as query params for .get() to alphavantage. 
+  Promise.all([validSymbol, validInterval]).then(async () => {
+    const data: AxiosResponse = await alphavantage
+      .get("", {
+        params: {
+          symbol: `${symbol}`,
+          market: "USD",
+          interval: `${interval}`,
+          apikey: alphaKey,
+        },
+      })
+      .then((data) => {
+        return data;
+      });
+    return data;
+  });
   //TODO: CATCH
 };
-
 
 //intercepts response from API and reshapes response so that data can be utilized in the client with minimal operations
 //TODO: HANDLE ERRORS
@@ -70,10 +74,10 @@ alphavantage.interceptors.response.use(async (response) => {
   let objectValues: object[] = reshapeObject(target);
   //remove volume from objectValues and return volume array
   let volumeArray: number[] = getVolumeArrayFromOhlcv(objectValues);
-  //package date array alongside objectValues, return array of arrays w/ shape: [number, array [strings]] 
+  //package date array alongside objectValues, return array of arrays w/ shape: [number, array [strings]]
   let formattedOhlc: any[][] = _.zip(formattedDate, objectValues);
   //Package Ohlc data and volume together in object that conforms to specified interface
-  let formattedData: ohlcvResponse = { formattedOhlc, volumeArray }
+  let formattedData: ohlcvResponse = { formattedOhlc, volumeArray };
   //Hand it over to the client
-  return formattedData 
+  return formattedData;
 });
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,6 +1,12 @@
 
-const validate = async (symbol: string): Promise<boolean> => {
+export const validateSymbol = async (symbol: string): Promise<boolean> => {
    const validSymbols: string[] = ['SOL', 'BTC', 'ETH', 'DOT', 'ALGO']
     const isValid: boolean = validSymbols.some(validToken => validToken === symbol)
     return isValid
 }
+
+export const validateInterval = async (symbol: string): Promise<boolean> => {
+    const validSymbols: string[] = ['1min', '5min', '15min', '30min', '60min']
+     const isValid: boolean = validSymbols.some(validToken => validToken === symbol)
+     return isValid
+ }
